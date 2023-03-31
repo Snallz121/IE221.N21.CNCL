e8acc21e0907f43c2aa33d256fa8a499e1422b36
Snallz121 <tongphucthinh041102@gmail.com>
2023-03-31 09:35:44 +0700

    8th changes

---
+++ Wecode/BT Function trên lớp/SortBySecondChar.py
@@ -0,0 +1,9 @@
+lst=["otter","whale","goose","chipmunk","fox","sheep","rabbit","marten"]
+InputText = lst
+InputText.sort(key = lambda x : x[1])
+print(InputText)
+
+lst=[(19542209,"NewYork"),(4887871,"Alabama"),(1420491,"Hawaii"),(626299,"Vermont"),(1805832,"WestVirginia"),(39865590,"California")]
+InputText = lst
+InputText.sort(key = lambda x : x[1])
+print(InputText)
